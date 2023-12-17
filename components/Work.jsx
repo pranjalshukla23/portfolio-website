10a721613be47ef1c2c53b43045f123db57be2b2
pranjal-betaflux <pranjal.shukla@betaflux.co>
2023-12-17 17:17:15 +0530

    UI changes

--- components/Work.jsx
+++ components/Work.jsx
@@ -37,7 +37,7 @@ export const Work = ({ projects }) => {
         <h1 className='font-Anton text-2xl md:text-4xl tracking-widest uppercase'>
           Noteworthy Projects
         </h1>
-        <div className='text-white w-2/3 flex justify-end items-center text-gray-400 mt-4 px-0 md:px-12 '>
+        {/* <div className='text-white w-2/3 flex justify-end items-center text-gray-400 mt-4 px-0 md:px-12 '>
           <p className='flex gap-4'>
             Filter by{" "}
             <button
@@ -54,7 +54,7 @@ export const Work = ({ projects }) => {
               Mobile Apps
             </button>
           </p>
-        </div>
+        </div> */}
         <div className='grid grid-cols-1 md:grid-cols-3 w-full md:w-4/5 p-8 gap-16'>
           {displayProjects &&
             displayProjects.map((project) => (
